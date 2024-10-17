138ca0d49883846b6f2ab8619715cf0d024a604a
Edelito Ortet <dyortet1@gmail.com>
2024-10-17 18:32:36 +0100

    Update jogo_par_ou_impar.py

--- jogo_par_ou_impar.py
+++ jogo_par_ou_impar.py
@@ -5,33 +5,33 @@ print(' '*10,'\033[1;34mJOGO DO PAR OU IMPAR\033[m')
 print('\033[1;33m-=-\033[m' * 15)
 
 while True:
-    numero = int(input('\033[1mEscolhe um número de 0 a 10: '))
+    jogador = int(input('\033[1mEscolhe um número de 0 a 10: \033[m'))
     sua_jogada = input('PAR ou IMPAR? [P/I]: ').upper().strip()
     computador = random.randrange(11)
-    total = numero + computador
+    total = jogador + computador
     if total % 2 == 0:
         if sua_jogada == 'P':
             print('\033[1;32mVOCÊ VENCEU!\033[m')
-            print(f'\033[1mVocê escolheu {numero} e o computador escolheu {computador}')
+            print(f'\033[1mVocê escolheu {jogador} e o computador escolheu {computador}')
             print(f'O resultado é {total} e é\033[m \033[1;32mPAR\033[m')
             conta_vitorias += 1
 
         elif sua_jogada == 'I':
             print('\033[1;31mVOCÊ PERDEU\033[m')
-            print(f'\033[1mVocê escolheu {numero} e o computador escolheu {computador}')
+            print(f'\033[1mVocê escolheu {jogador} e o computador escolheu {computador}')
             print(f'O resultado é {total} e é\033[m \033[1;31mPAR\033[m')
             break
 
     elif total % 2 == 1:
         if sua_jogada == 'I':
             print('\033[1;32mVOCÊ VENCEU\033[m')
-            print(f'\033[1mVocê escolheu {numero} e o computador escolheu {computador}')
+            print(f'\033[1mVocê escolheu {jogador} e o computador escolheu {computador}')
             print(f'O resultado é {total} e é\033[m \033[1;32mÍMPAR\033[m')
             conta_vitorias += 1
 
         elif sua_jogada == 'P':
             print('\033[1;31mVOCÊ PERDEU\033[m')
-            print(f'\033[1mVocê escolheu {numero} e o computador escolheu {computador}')
+            print(f'\033[1mVocê escolheu {jogador} e o computador escolheu {computador}')
             print(f'O resultado é {total} e é\033[m \033[1;31mÍMPAR\033[m')
             break
 
